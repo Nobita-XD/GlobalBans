ca870e7ac6679ce21c34b1046e29f4cb8a9fe0c2
ABHISHEK THAKUR <99659192+Nobita-XD@users.noreply.github.com>
2022-06-08 23:05:41 +0530

    Update Ban.py

--- Abhi/Plugins/Ban.py
+++ Abhi/Plugins/Ban.py
@@ -1,1 +1,58 @@
+from Abhi import bot
+from pyrogram import filters
+from Abhi.Core.admin_check import authorized_users_only
+from pyrogram.types import Message
+from Abhi.Core.extract import extract
+from time import time
 
+admins_in_chat = {}
+
+
+async def list_admins(chat_id: int):
+    global admins_in_chat
+    if chat_id in admins_in_chat:
+        interval = time() - admins_in_chat[chat_id]["last_updated_at"]
+        if interval < 3600:
+            return admins_in_chat[chat_id]["data"]
+
+    admins_in_chat[chat_id] = {
+        "last_updated_at": time(),
+        "data": [
+            member.user.id
+            async for member in app.iter_chat_members(
+                chat_id, filter="administrators"
+            )
+        ],
+    }
+    return admins_in_chat[chat_id]["data"]
+
+
+@app.on_message(filters.command("ban") & ~filters.edited & ~filters.private)
+@authorized_users_only
+async def banFunc(_, message: Message):
+    user_id, reason = await extract(message, sender_chat=True)
+
+    if not user_id:
+        return await message.reply_text("I can't find that user.")
+    if user_id in (await list_admins(message.chat.id)):
+        return await message.reply_text(
+            "I can't ban an admin, You know the rules, so do i."
+        )
+
+    try:
+        mention = (await app.get_users(user_id)).mention
+    except IndexError:
+        mention = (
+            message.reply_to_message.sender_chat.title
+            if message.reply_to_message
+            else "Anon"
+        )
+
+    msg = (
+        f"**Banned User:** {mention}\n"
+        f"**Banned By:** {message.from_user.mention if message.from_user else 'Anon'}\n"
+    )
+    if reason:
+        msg += f"**Reason:** {reason}"
+    await message.chat.ban_member(user_id)
+    await message.reply_text(msg)
