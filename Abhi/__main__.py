fb97c58ea8d07c435e531afb063b3bb10bc8951a
ABHISHEK THAKUR <99659192+Nobita-XD@users.noreply.github.com>
2022-06-09 08:43:04 +0530

    Update __main__.py

--- Abhi/__main__.py
+++ Abhi/__main__.py
@@ -1,7 +1,7 @@
 import importlib
 import asyncio
 from contextlib import closing, suppress
-
+from pyrogram import idle
 from uvloop import install
 from Abhi import GBAN_LOG_GROUP_ID, app, log
 from Abhi.Plugins import ALL_MODULES
