84b8e78ea085c6e3b3456e7bab9dab259ba332ba
ABHISHEK THAKUR <99659192+Nobita-XD@users.noreply.github.com>
2022-06-09 07:43:42 +0530

    Update __init__.py

--- Abhi/__init__.py
+++ Abhi/__init__.py
@@ -1,8 +1,27 @@
 from os import environ
 from pyrogram import Client
+import time
 
 
+class Log:
+    def __init__(self, save_to_file=False, file_name="wbb.log"):
+        self.save_to_file = save_to_file
+        self.file_name = file_name
 
+    def info(self, msg):
+        print(f"[+]: {msg}")
+        if self.save_to_file:
+            with open(self.file_name, "a") as f:
+                f.write(f"[INFO]({time.ctime(time.time())}): {msg}\n")
+
+    def error(self, msg):
+        print(f"[-]: {msg}")
+        if self.save_to_file:
+            with open(self.file_name, "a") as f:
+                f.write(f"[ERROR]({time.ctime(time.time())}): {msg}\n")
+
+
+log = Log(True, "bot.log")
 
 
 BOT_TOKEN = environ.get("BOT_TOKEN", None)
