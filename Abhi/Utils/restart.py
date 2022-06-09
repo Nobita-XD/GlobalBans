f7079ea7e84a6fd37b61083fa765c762939c2baa
ABHISHEK THAKUR <99659192+Nobita-XD@users.noreply.github.com>
2022-06-09 08:04:28 +0530

    Create restart.py

---
+++ Abhi/Utils/restart.py
@@ -0,0 +1,15 @@
+from typing import Dict
+from Abhi.Database import db
+
+
+restart_stagedb = db.restart_stage
+
+async def clean_restart_stage() -> dict:
+    data = await restart_stagedb.find_one({"something": "something"})
+    if not data:
+        return {}
+    await restart_stagedb.delete_one({"something": "something"})
+    return {
+        "chat_id": data["chat_id"],
+        "message_id": data["message_id"],
+    }
