0ca3f5cda0f7af09ae4c919a50b597617fa84b76
ABHISHEK THAKUR <99659192+Nobita-XD@users.noreply.github.com>
2022-06-09 08:01:50 +0530

    Update __main__.py

--- Abhi/__main__.py
+++ Abhi/__main__.py
@@ -1,1 +1,79 @@
+import importlib
+import asyncio
+from contextlib import closing, suppress
 
+from uvloop import install
+from Abhi import GBAN_LOG_GROUP_ID, app, log
+from Abhi.Plugins import ALL_MODULES
+from Abhi.Utils.restart import clean_restart_stage
+
+loop = asyncio.get_event_loop()
+
+HELPABLE = {}
+
+
+async def start_bot():
+    global HELPABLE
+
+    for module in ALL_MODULES:
+        imported_module = importlib.import_module("Abhi.Plugins." + module)
+        if (
+                hasattr(imported_module, "__MODULE__")
+                and imported_module.__MODULE__
+        ):
+            imported_module.__MODULE__ = imported_module.__MODULE__
+            if (
+                    hasattr(imported_module, "__HELP__")
+                    and imported_module.__HELP__
+            ):
+                HELPABLE[
+                    imported_module.__MODULE__.replace(" ", "_").lower()
+                ] = imported_module
+    bot_modules = ""
+    j = 1
+    for i in ALL_MODULES:
+        if j == 4:
+            bot_modules += "|{:<15}|\n".format(i)
+            j = 0
+        else:
+            bot_modules += "|{:<15}".format(i)
+        j += 1
+    print("+===============================================================+")
+    print("|                          GLOBAL BANS                              |")
+    print("+===============+===============+===============+===============+")
+    print(bot_modules)
+    print("+===============+===============+===============+===============+")
+    log.info(f"BOT STARTED AS {}!")
+    
+
+    restart_data = await clean_restart_stage()
+
+    try:
+        log.info("Sending online status")
+        if restart_data:
+            await app.edit_message_text(
+                restart_data["chat_id"],
+                restart_data["message_id"],
+                "**Restarted Successfully**",
+            )
+
+        else:
+            await app.send_message(GBAN_LOG_GROUP_ID, "Bot started!")
+    except Exception:
+        pass
+
+    await idle()
+    
+    await app.stop()
+    log.info("Cancelling asyncio tasks")
+    for task in asyncio.all_tasks():
+        task.cancel()
+    log.info("Dead!")
+    
+    
+if __name__ == "__main__":
+    install()
+    with closing(loop):
+        with suppress(asyncio.exceptions.CancelledError):
+            loop.run_until_complete(start_bot())
+        loop.run_until_complete(asyncio.sleep(3.0))
