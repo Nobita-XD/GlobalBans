d3ddac4b3139eaaa8c3fffce004927973c15bc8e
ABHISHEK THAKUR <99659192+Nobita-XD@users.noreply.github.com>
2022-06-09 09:38:47 +0530

    Update __main__.py

--- Abhi/__main__.py
+++ Abhi/__main__.py
@@ -7,17 +7,5 @@ from Abhi import GBAN_LOG_GROUP_ID, app, log
 from Abhi.Plugins import ALL_MODULES
 from Abhi.Utils.restart import clean_restart_stage
 
-loop = asyncio.get_event_loop()
-
-async def init():
-    await app.start()
-    for all_module in ALL_MODULES:
-        importlib.import_module("Abhi.Plugins." + all_module)
-    log.info(
-        "Successfully Imported Modules "
-    )
-    
-if __name__ == "__main__":
-    loop.run_until_complete(init())
-    log.info("Stopping Gban Bot! GoodBye")
-    
+log.info("Bot Started")
+app.start()
