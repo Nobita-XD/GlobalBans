f5cdc0403dd44b4f9d5708153255232b5dac6679
ABHISHEK THAKUR <99659192+Nobita-XD@users.noreply.github.com>
2022-06-11 07:59:57 +0530

    Update __init__.py

--- Abhi/__init__.py
+++ Abhi/__init__.py
@@ -30,10 +30,11 @@ API_HASH = environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
 OWNER_ID = [int(x) for x in environ.get("OWNER_ID", "").split()]
 MONGO_URL = environ.get("MONGO_URL", None)
 GBAN_LOG_GROUP_ID = int(environ.get("LOG_GROUP_ID", None))
-app = Client(
-    "Global Bans",
-    API_ID,
-    API_HASH,
-    bot_token=BOT_TOKEN,
-    plugins=dict(root="Abhi.Plugins"),
-)
+
+app = Client( 
+     "Global Bans", 
+     api_id=API_ID, 
+     api_hash=API_HASH, 
+     bot_token=BOT_TOKEN, 
+     plugins=dict(root="Abhi.Plugins"), 
+ )
